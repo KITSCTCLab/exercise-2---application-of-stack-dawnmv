478e614861de64dc07dfddcfd235082985687e01
URK21CS2014DAWNMATHEWVARGHESE <109648022+URK21CS2014DAWNMATHEWVARGHESE@users.noreply.github.com>
2022-08-02 22:06:07 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -26,9 +26,9 @@ class Evaluate:
       True if it is empty, else returns False.
     """
       # Write your code here
-     if self.top == -1:
+       if self.top == -1:
          return True
-      else:
+       else:
          return False
 
 
